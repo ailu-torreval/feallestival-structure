b896ae5f210fab6c61a78c18f2a463ba59d0ef29
ailu-torreval <89412378+ailu-torreval@users.noreply.github.com>
2022-05-17 14:53:22 +0200

    Add files via upload

---
+++ structure.js
@@ -0,0 +1,125 @@
+<App>
+    <LandingPage>
+    <HeroSection>
+        <Header>
+            <PrimBtn>Open App</PrimBtn>
+            <HeaderBg></HeaderBg>
+            <Logo></Logo>
+            <FestName></FestName>
+        </Header>
+        <Timer></Timer>
+        <HeroButtons>
+            <SecBtn>Lineup</SecBtn>
+            <SectBtn>Tickets</SectBtn>
+        </HeroButtons>
+        <ScrollElm></ScrollElm>
+    </HeroSection>
+    <LineUpSection>
+        <FullLineUp></FullLineUp>
+        <MoreArtists>
+            <p>And More...</p>
+            <ScrollElm></ScrollElm>
+        </MoreArtists>
+        <PrimBtn>Full Line Up</PrimBtn>
+    </LineUpSection>
+    <BannerSection></BannerSection>
+    <FaqSection>
+        <h2>Are You Gonna Miss It?</h2>
+        <FaqBtns>
+            <PrimBtn>FAQ</PrimBtn>
+            <PrimBtn>Get Your Tickets</PrimBtn>
+        </FaqBtns>
+    </FaqSection>
+    </LandingPage>
+    <LineupPage>
+        <NavBar>
+            <Logo></Logo>
+            <FestName></FestName>
+            <PrimBtn>LogIn</PrimBtn>
+            <PrimBtn>Tickets</PrimBtn>
+        </NavBar>
+        <Main>
+            <Breadcrumbs>Back to main page</Breadcrumbs>
+            <HeaderBg></HeaderBg>
+            <MainTitle>LineUp</MainTitle>
+        </Main>
+        <CardGrid>
+            <DayCard></DayCard>
+            <DayCard></DayCard>
+            <DayCard></DayCard>
+            <DayCard></DayCard>
+            <DayCard></DayCard>
+            <DayCard></DayCard>
+            <DayCard></DayCard>
+        </CardGrid>
+    </LineupPage>
+    <TicketsPage>
+    <NavBar>
+            <Logo></Logo>
+            <FestName></FestName>
+            <PrimBtn>LogIn</PrimBtn>
+            <PrimBtn>Tickets</PrimBtn>
+        </NavBar>
+        <Main>
+            <Breadcrumbs>Back to main page</Breadcrumbs>
+            <HeaderBg></HeaderBg>
+            <MainTitle>Tickets</MainTitle>
+        </Main>
+        <CardGrid>
+        <TicketCard></TicketCard>
+        <TicketCard></TicketCard>
+        <TicketCard></TicketCard>
+        </CardGrid>
+        <FaqSection>
+            BUT WITHOUT BUY TICKETS BUTTON
+        </FaqSection>
+        <OrderForm>
+            <OrderBreadcrumbs>
+            <OrderSteps></OrderSteps>
+            </OrderBreadcrumbs>
+            <Form>
+                <TicketInfo>
+                    <FormTitle>Select Your Tickets</FormTitle>
+                    <TicketSelection></TicketSelection>
+                    <SelectTicketType></SelectTicketType>
+                    <p>Kids under 12 dont pay bla bla bla</p>
+                </TicketInfo>
+                <PersonalInfo>
+                    <FormTitle>Personal Info</FormTitle>
+                    <GuestInfo></GuestInfo>
+                    <GuestInfo></GuestInfo>
+                    <GuestInfo></GuestInfo>
+                    <GuestInfo></GuestInfo>
+                </PersonalInfo>
+                <CampingInfo>
+                    <FormTitle>Camping Info</FormTitle>
+                    <h3>Choose camping area</h3>
+                    <Map></Map>
+                    <BCard></BCard>
+                </CampingInfo>
+                <AddOns>
+                    <FormTitle>Get AddOns</FormTitle>
+                    <CardGrid>
+                    <BCard></BCard>
+                    <BCard></BCard>
+                    <BCard></BCard>
+                    </CardGrid>
+                    <FormTitle>Your Faellescard</FormTitle>
+                    <FaellesCardContainer>
+                        <FaellesCard></FaellesCard>
+                        
+                    </FaellesCardContainer>
+                </AddOns>
+                <CheckOut></CheckOut>
+                <TransactionCompleted></TransactionCompleted>
+            </Form>
+            <OrderSummary>
+                <Summary></Summary>
+                <OrderTotal></OrderTotal>
+                <PrimBtn>Next Step</PrimBtn>
+            </OrderSummary>
+        </OrderForm>
+    </TicketsPage>
+
+    <Footer></Footer>
+</App>
