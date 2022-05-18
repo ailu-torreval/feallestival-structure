e8a1c251730781c120f4cd58e14c592dacb27f5e
ailin torre val <aili0057@stud.kea.dk>
2022-05-18 17:55:18 +0200

    finished

--- structure.js
+++ structure.js
@@ -1,125 +1,203 @@
 <App>
-    <LandingPage>
+  <LandingPage>
     <HeroSection>
-        <Header>
-            <PrimBtn>Open App</PrimBtn>
-            <HeaderBg></HeaderBg>
-            <Logo></Logo>
-            <FestName></FestName>
-        </Header>
-        <Timer></Timer>
-        <HeroButtons>
-            <SecBtn>Lineup</SecBtn>
-            <SectBtn>Tickets</SectBtn>
-        </HeroButtons>
-        <ScrollElm></ScrollElm>
+      <Header>
+        <PrimBtn>Open App</PrimBtn>
+        <HeaderBg></HeaderBg>
+        <Logo></Logo>
+        <HeroTitle>Faellestival</HeroTitle>
+      </Header>
+      <Timer></Timer>
+      <HeroButtons>
+        <SecBtn>Lineup</SecBtn>
+        <SectBtn>Tickets</SectBtn>
+      </HeroButtons>
+      <ScrollElm></ScrollElm>
     </HeroSection>
     <LineUpSection>
-        <FullLineUp></FullLineUp>
-        <MoreArtists>
-            <p>And More...</p>
-            <ScrollElm></ScrollElm>
-        </MoreArtists>
-        <PrimBtn>Full Line Up</PrimBtn>
+      <FullLineUp></FullLineUp>
+      <MoreArtists>
+        <p>And More...</p>
+        <ScrollElm></ScrollElm>
+      </MoreArtists>
+      <PrimBtn>Full Line Up</PrimBtn>
     </LineUpSection>
     <BannerSection></BannerSection>
     <FaqSection>
-        <h2>Are You Gonna Miss It?</h2>
-        <FaqBtns>
-            <PrimBtn>FAQ</PrimBtn>
-            <PrimBtn>Get Your Tickets</PrimBtn>
-        </FaqBtns>
+      <h2>Are You Gonna Miss It?</h2>
+      <FaqBtns>
+        <PrimBtn>FAQ</PrimBtn>
+        <PrimBtn>Get Your Tickets</PrimBtn>
+      </FaqBtns>
     </FaqSection>
-    </LandingPage>
-    <LineupPage>
-        <NavBar>
-            <Logo></Logo>
-            <FestName></FestName>
-            <PrimBtn>LogIn</PrimBtn>
-            <PrimBtn>Tickets</PrimBtn>
-        </NavBar>
-        <Main>
-            <Breadcrumbs>Back to main page</Breadcrumbs>
-            <HeaderBg></HeaderBg>
-            <MainTitle>LineUp</MainTitle>
-        </Main>
-        <CardGrid>
-            <DayCard></DayCard>
-            <DayCard></DayCard>
-            <DayCard></DayCard>
-            <DayCard></DayCard>
-            <DayCard></DayCard>
-            <DayCard></DayCard>
-            <DayCard></DayCard>
-        </CardGrid>
-    </LineupPage>
-    <TicketsPage>
+  </LandingPage>
+  <LineupPage>
     <NavBar>
-            <Logo></Logo>
-            <FestName></FestName>
-            <PrimBtn>LogIn</PrimBtn>
-            <PrimBtn>Tickets</PrimBtn>
-        </NavBar>
-        <Main>
-            <Breadcrumbs>Back to main page</Breadcrumbs>
-            <HeaderBg></HeaderBg>
-            <MainTitle>Tickets</MainTitle>
-        </Main>
-        <CardGrid>
-        <TicketCard></TicketCard>
-        <TicketCard></TicketCard>
-        <TicketCard></TicketCard>
-        </CardGrid>
-        <FaqSection>
-            BUT WITHOUT BUY TICKETS BUTTON
-        </FaqSection>
-        <OrderForm>
-            <OrderBreadcrumbs>
-            <OrderSteps></OrderSteps>
-            </OrderBreadcrumbs>
-            <Form>
-                <TicketInfo>
-                    <FormTitle>Select Your Tickets</FormTitle>
-                    <TicketSelection></TicketSelection>
-                    <SelectTicketType></SelectTicketType>
-                    <p>Kids under 12 dont pay bla bla bla</p>
-                </TicketInfo>
-                <PersonalInfo>
-                    <FormTitle>Personal Info</FormTitle>
-                    <GuestInfo></GuestInfo>
-                    <GuestInfo></GuestInfo>
-                    <GuestInfo></GuestInfo>
-                    <GuestInfo></GuestInfo>
-                </PersonalInfo>
-                <CampingInfo>
-                    <FormTitle>Camping Info</FormTitle>
-                    <h3>Choose camping area</h3>
-                    <Map></Map>
-                    <BCard></BCard>
-                </CampingInfo>
-                <AddOns>
-                    <FormTitle>Get AddOns</FormTitle>
-                    <CardGrid>
-                    <BCard></BCard>
-                    <BCard></BCard>
-                    <BCard></BCard>
-                    </CardGrid>
-                    <FormTitle>Your Faellescard</FormTitle>
-                    <FaellesCardContainer>
-                        <FaellesCard></FaellesCard>
-                        
-                    </FaellesCardContainer>
-                </AddOns>
-                <CheckOut></CheckOut>
-                <TransactionCompleted></TransactionCompleted>
-            </Form>
-            <OrderSummary>
-                <Summary></Summary>
-                <OrderTotal></OrderTotal>
-                <PrimBtn>Next Step</PrimBtn>
-            </OrderSummary>
-        </OrderForm>
-    </TicketsPage>
+      <Logo></Logo>
+      <FestName></FestName>
+      <PrimBtn>LogIn</PrimBtn>
+      <PrimBtn>Tickets</PrimBtn>
+    </NavBar>
+    <Main>
+      <Breadcrumbs>Back to main page</Breadcrumbs>
+      <HeaderBg></HeaderBg>
+      <MainTitle>LineUp</MainTitle>
+    </Main>
+    <CardGrid>
+      <DayCard></DayCard>
+      <DayCard></DayCard>
+      <DayCard></DayCard>
+      <DayCard></DayCard>
+      <DayCard></DayCard>
+      <DayCard></DayCard>
+      <DayCard></DayCard>
+    </CardGrid>
+  </LineupPage>
+  <TicketsPage>
+    <NavBar>
+      <Logo></Logo>
+      <FestName></FestName>
+      <PrimBtn>LogIn</PrimBtn>
+      <PrimBtn>Tickets</PrimBtn>
+    </NavBar>
+    <Main>
+      <Breadcrumbs>Back to main page</Breadcrumbs>
+      <HeaderBg></HeaderBg>
+      <MainTitle>Tickets</MainTitle>
+    </Main>
+    <CardGrid>
+      <TicketCard></TicketCard>
+      <TicketCard></TicketCard>
+      <TicketCard></TicketCard>
+    </CardGrid>
+    <FaqSection>BUT WITHOUT BUY TICKETS BUTTON</FaqSection>
+    <OrderForm>
+      <OrderBreadcrumbs>
+        <OrderSteps></OrderSteps>
+      </OrderBreadcrumbs>
+      <Form>
+        <TicketInfo>
+          <FormTitle>Select Your Tickets</FormTitle>
+          <TicketSelection></TicketSelection>
+          <SelectTicketType></SelectTicketType>
+          <p>Kids under 12 dont pay bla bla bla</p>
+        </TicketInfo>
+        <PersonalInfo>
+          <FormTitle>Personal Info</FormTitle>
+          <GuestInfo></GuestInfo>
+          <GuestInfo></GuestInfo>
+          <GuestInfo></GuestInfo>
+          <GuestInfo></GuestInfo>
+        </PersonalInfo>
+        <CampingInfo>
+          <FormTitle>Camping Info</FormTitle>
+          <h3>Choose camping area</h3>
+          <Map></Map>
+          <BCard></BCard>
+        </CampingInfo>
+        <AddOns>
+          <FormTitle>Get AddOns</FormTitle>
+          <CardGrid>
+            <BCard></BCard>
+            <BCard></BCard>
+            <BCard></BCard>
+          </CardGrid>
+          <FormTitle>Your Faellescard</FormTitle>
+          <FaellesCardContainer>
+            <FaellesCard></FaellesCard>
+          </FaellesCardContainer>
+        </AddOns>
+        <CheckOut></CheckOut>
+        <TransactionCompleted></TransactionCompleted>
+      </Form>
+      <OrderSummary>
+        <Summary></Summary>
+        <OrderTotal></OrderTotal>
+        <SecBtn>Previous Step</SecBtn>
+        <PrimBtn>Next Step</PrimBtn>
+      </OrderSummary>
+    </OrderForm>
+  </TicketsPage>
 
+  <Footer></Footer>
+  <FestApp>
+    <NavBar>BUT WITH GUEST NAME AND BURGER MENU</NavBar>
+    <FestLandPage>
+      <Header>
+        BUT WITHOUT FEST BUTTON AND WITH NAME AND BURGER MENU BUTTON
+      </Header>
+      <BurgerMenu></BurgerMenu>
+      <PlayingNow>
+        <SectionTitle>Who is playing right now?</SectionTitle>
+        <CardGrid>
+          <CCard></CCard>
+        </CardGrid>
+      </PlayingNow>
+      <Map>
+        <PrimBtn>Fin your way</PrimBtn>
+      </Map>
+      <FaellesCardSection>
+        <SectionTitle>Getting Hungry?</SectionTitle>
+        <PrimBtn>TopUp your card</PrimBtn>
+        <SubText>Dont have one? buy it here</SubText>
+      </FaellesCardSection>
+      <ArtistSection>
+        <SectionTitle>Artists</SectionTitle>
+        <CardGrid>
+          <ScrollElm></ScrollElm>
+          <ArtistCard></ArtistCard>
+          <ArtistCard></ArtistCard>
+          <ArtistCard></ArtistCard>
+          <ArtistCard></ArtistCard>
+          <ScrollElm></ScrollElm>
+        </CardGrid>
+      </ArtistSection>
+    </FestLandPage>
+    <ArtistPage>
+      <ArtistHeader>
+        <Logo></Logo>
+        <HeroTitle>ARTIST NAME</HeroTitle>
+        <CCard></CCard>
+      </ArtistHeader>
+      <ArtistBio></ArtistBio>
+      <ArtistSection>
+        <SectionTitle>See More Artists</SectionTitle>
+        <CardGrid>
+          <ScrollElm></ScrollElm>
+          <ArtistCard></ArtistCard>
+          <ArtistCard></ArtistCard>
+          <ArtistCard></ArtistCard>
+          <ArtistCard></ArtistCard>
+          <ScrollElm></ScrollElm>
+        </CardGrid>
+      </ArtistSection>
+    </ArtistPage>
+    <ProgramPage>
+        <SectionTitle>MAP/PROGRAM</SectionTitle>
+        <Map></Map>
+        <ProgramSection>
+        <FilterControls>
+            <PrimBtn></PrimBtn>
+        </FilterControls>
+        <ProgramTable></ProgramTable>
+        </ProgramSection>
+    </ProgramPage>
+    <FaellesCardPage>
+        <HeaderTitle>Your Faellescard</HeaderTitle>
+        <CardGrid>
+        <FaellesCard></FaellesCard>
+        <CardControls>
+            <Topup></Topup>
+            <BtnGrid>
+                <PrimBtn>Use saved Card</PrimBtn>
+                <PrimBtn>Use New Card</PrimBtn>
+            </BtnGrid>
+            <CheckOut>BUT WITH THE SAVE YOUR CARD OPTION</CheckOut>
+            <PrimBtn>Buy</PrimBtn>
+            <TopupCompleted></TopupCompleted>
+        </CardControls>
+        </CardGrid>
+    </FaellesCardPage>
     <Footer></Footer>
-</App>
+  </FestApp>
+</App>;
